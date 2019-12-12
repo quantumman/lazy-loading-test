2160dfd76791c40c471517691d95a9718e1d04de
Syoudai Yokoyama <>
2019-12-12 20:13:32 +0900

    Add store

---
+++ src/modules/api.js
@@ -0,0 +1,21 @@
+// @flow
+
+// State
+
+
+type State = {};
+
+const initialState: State = {};
+
+// Action
+
+type Action = {}
+
+// Reducer
+
+export default (state: State = initialState, action: Action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
